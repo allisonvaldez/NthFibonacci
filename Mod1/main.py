ff68f26810c32c9d3a22781d6e8d138c692c45bd
Allison Valdez <ajsnipes@usfca.edu>
2022-01-05 11:22:01 -0800

    finished project

--- Mod1/main.py
+++ Mod1/main.py
@@ -1,16 +1,19 @@
 """
-time complexity: O(2^n) due to the nature of recursive nature of binary tree
+time: O(2^n) due to the nature of recursive nature of fibonacci calls
 space complexity: O(n) depending on the length of the array
 """
 
 
 def get_nth_fibonacci(num):
+    # base case to calculate second number in array
     if num == 2:
         return 1
+    # base case to calculate first number in array
     elif num == 1:
         return 0
+    # how to calculate remaining numbers of the array
     else:
         return get_nth_fibonacci(num - 1) + get_nth_fibonacci(num - 2)
 
 
-print(get_nth_fibonacci(4))
+print(get_nth_fibonacci(10))
